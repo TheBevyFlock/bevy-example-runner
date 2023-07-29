831b8b3f1b5d15d02781aa067d18564971d25fc4
François <mockersf@gmail.com>
2023-07-29 02:24:15 +0200

    deploy a site?

---
+++ src/main.rs
@@ -0,0 +1,116 @@
+use chrono::NaiveDateTime;
+use percy::read_percy_results;
+use serde::Serialize;
+use std::{
+    collections::{HashMap, HashSet},
+    fs, thread,
+    time::Duration,
+};
+use tera::{Context, Tera};
+
+mod percy;
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Serialize)]
+struct Example {
+    name: String,
+    category: String,
+}
+
+#[derive(Debug, Serialize, Default)]
+struct Run {
+    date: String,
+    commit: String,
+    results: HashMap<String, HashMap<String, String>>,
+    screenshots: HashMap<String, HashMap<String, String>>,
+}
+
+fn main() {
+    let paths = fs::read_dir(std::env::args().nth(1).as_deref().unwrap()).unwrap();
+
+    let mut all_examples = HashSet::new();
+    let mut runs = vec![];
+
+    let mut folders = paths
+        .filter_map(|dir| dir.map(|d| d.path()).ok())
+        .collect::<Vec<_>>();
+    folders.sort();
+    folders.reverse();
+
+    for path in folders {
+        println!("Processing {:?}", path);
+        let mut split = path.file_name().unwrap().to_str().unwrap().split('-');
+        let mut run = Run {
+            date: NaiveDateTime::parse_from_str(split.next().unwrap(), "%Y%m%d%H%M")
+                .unwrap()
+                .format("%Y-%m-%d %H:%M")
+                .to_string(),
+            commit: split.next().unwrap().to_string(),
+            ..Default::default()
+        };
+
+        for file in fs::read_dir(path).unwrap() {
+            let path = file.as_ref().unwrap().path();
+            let mut name = path.file_name().unwrap().to_str().unwrap().split('-');
+            let platform = name.next().unwrap();
+            let kind = name.next().unwrap();
+
+            if ["successes", "failures", "no_screenshots"].contains(&kind) {
+                fs::read_to_string(file.as_ref().unwrap().path())
+                    .unwrap()
+                    .lines()
+                    .for_each(|line| {
+                        let mut line = line.split(" - ");
+                        let mut details = line.next().unwrap().split('/');
+                        let example = Example {
+                            category: details.next().unwrap().to_string(),
+                            name: details.next().unwrap().to_string(),
+                        };
+                        all_examples.insert(example.clone());
+                        run.results
+                            .entry(example.name)
+                            .or_insert_with(HashMap::new)
+                            .insert(platform.to_string(), kind.to_string());
+                    });
+            }
+            if kind == "percy" {
+                let screenshots =
+                    read_percy_results(fs::read_to_string(file.as_ref().unwrap().path()).unwrap());
+                thread::sleep(Duration::from_secs(10));
+                for (example, screenshot) in screenshots.into_iter() {
+                    run.screenshots
+                        .entry(
+                            example
+                                .split('/')
+                                .last()
+                                .unwrap()
+                                .to_string()
+                                .split('.')
+                                .next()
+                                .unwrap()
+                                .to_string(),
+                        )
+                        .or_insert_with(HashMap::new)
+                        .insert(platform.to_string(), screenshot);
+                }
+            }
+        }
+        runs.push(run);
+    }
+
+    let mut sorted_examples = all_examples.iter().collect::<Vec<_>>();
+    sorted_examples.sort_by_key(|a| format!("{}/{}", a.category, a.name));
+
+    let mut context = Context::new();
+    context.insert("runs".to_string(), &runs);
+    context.insert("all_examples".to_string(), &sorted_examples);
+
+    let mut tera = Tera::default();
+    tera.add_raw_template(
+        "index.html",
+        &std::fs::read_to_string("./templates/index.html").unwrap(),
+    )
+    .unwrap();
+    let rendered = tera.render("index.html", &context).unwrap();
+
+    std::fs::write("./index.html", &rendered).unwrap();
+}
---
+++ src/percy.rs
@@ -0,0 +1,179 @@
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "kebab-case")]
+pub struct Main {
+    pub web_url: String,
+    pub total_comparisons: u32,
+    pub total_comparisons_diff: u32,
+}
+
+pub fn read_percy_results(results: String) -> Vec<(String, String)> {
+    let main: Main = serde_json::from_str(&results).unwrap();
+    let build_id = main.web_url.split('/').last().unwrap();
+    let data = ureq::get(&format!(
+        "https://percy.io/api/v1/builds/{}/snapshots",
+        build_id
+    ))
+    .call()
+    .unwrap()
+    .into_json::<SnapshotsData>();
+
+    if let Ok(data) = data {
+        snapshots_to_images(data)
+    } else {
+        println!("error parsing percy response");
+        vec![]
+    }
+}
+
+fn snapshots_to_images(snapshots: SnapshotsData) -> Vec<(String, String)> {
+    let mut images = Vec::new();
+    for snapshot in snapshots.data {
+        match snapshot {
+            Snapshot::Snapshots {
+                attributes,
+                relationships,
+                ..
+            } => {
+                if attributes.review_state_reason == "no_diffs" {
+                    let comparison_id = relationships.comparisons.data[0].id.clone();
+                    let comparison = snapshots
+                        .included
+                        .iter()
+                        .find_map(|included| match included {
+                            Snapshot::Comparisons { id, relationships } if id == &comparison_id => {
+                                Some(relationships)
+                            }
+                            _ => None,
+                        })
+                        .unwrap();
+                    let base_screenshot_id =
+                        comparison.base_screenshot.data.as_ref().unwrap().id.clone();
+                    let base_screenshot = snapshots
+                        .included
+                        .iter()
+                        .find_map(|included| match included {
+                            Snapshot::Screenshots { id, relationships }
+                                if id == &base_screenshot_id =>
+                            {
+                                Some(relationships)
+                            }
+                            _ => None,
+                        })
+                        .unwrap();
+                    let image_id = base_screenshot.image.data.as_ref().unwrap().id.clone();
+                    let image = snapshots
+                        .included
+                        .iter()
+                        .find_map(|included| match included {
+                            Snapshot::Images { id, attributes } if id == &image_id => {
+                                Some(attributes)
+                            }
+                            _ => None,
+                        })
+                        .unwrap();
+                    images.push((attributes.name, image.url.clone()));
+                }
+            }
+            _ => {}
+        }
+    }
+    images
+}
+
+#[derive(Deserialize, Debug)]
+struct SnapshotsData {
+    data: Vec<Snapshot>,
+    included: Vec<Snapshot>,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(tag = "type")]
+#[serde(rename_all = "kebab-case")]
+enum Snapshot {
+    Snapshots {
+        attributes: SnapshotAttributes,
+        relationships: SnapshotRelationship,
+    },
+    Images {
+        id: String,
+        attributes: ImageAttributes,
+    },
+    Comparisons {
+        id: String,
+        relationships: ComparisonRelationship,
+    },
+    Screenshots {
+        id: String,
+        relationships: ScreenshotRelationship,
+    },
+    Builds,
+    Browsers,
+    BrowserFamilies,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "kebab-case")]
+struct RelationshipDatas {
+    data: Vec<RelationshipData>,
+}
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "kebab-case")]
+struct RelationshipSingleData {
+    data: Option<RelationshipData>,
+}
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "kebab-case")]
+struct RelationshipData {
+    id: String,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "kebab-case")]
+struct SnapshotAttributes {
+    name: String,
+    review_state_reason: String,
+}
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "kebab-case")]
+struct SnapshotRelationship {
+    comparisons: RelationshipDatas,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "kebab-case")]
+struct ImageAttributes {
+    url: String,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "kebab-case")]
+struct ScreenshotRelationship {
+    image: RelationshipSingleData,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "kebab-case")]
+struct ComparisonRelationship {
+    base_screenshot: RelationshipSingleData,
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fs;
+
+    use crate::percy::{snapshots_to_images, SnapshotsData};
+
+    #[test]
+    fn read_file() {
+        let file = fs::read_to_string("src/test-percy.json").unwrap();
+        // dbg!(read_percy_results(file));
+        let read = serde_json::from_str::<SnapshotsData>(&file).unwrap();
+        dbg!(read.data.len());
+        dbg!(read.included.len());
+        dbg!(&read.data[0]);
+        dbg!(snapshots_to_images(read));
+        assert!(false);
+    }
+}
